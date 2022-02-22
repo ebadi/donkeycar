c139ceaab02535f18c597b347a462fc10657df50
Hamid Ebadi <hamid.ebadi@gmail.com>
2022-02-22 11:52:16 +0100

    Collect camera image

--- simple.py
+++ simple.py
@@ -1,15 +1,18 @@
 import gym
 import numpy as np
 import gym_donkeycar
-
+import cv2
 env = gym.make("donkey-warren-track-v0")
 
 obs = env.reset()
 try:
-    for _ in range(100):
-        action = np.array([0.0, 0.5])  
+    for _ in range(1000):
+        action = np.array([0.5, 0.5])  
         obs, reward, done, info = env.step(action)
-        print(obs)
+        # print(obs)
+        print(type(obs))
+        cv2.imshow("window name", obs[:, :, ::])
+        cv2.waitKey(-1)
 except KeyboardInterrupt:
     pass
 env.close()
